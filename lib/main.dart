7093b7952a48f83e94a7a5bbff4d9bdf296ec169
imabuza <iitmabuza@gmail.com>
2020-02-26 14:57:54 +0200

    info containers

--- lib/main.dart
+++ lib/main.dart
@@ -10,37 +10,72 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            //Makes it stretch to end to end
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(
-                  height: 100.00,
-                  width: 100.00,
+              CircleAvatar(
+                radius: 60.0,
+                backgroundImage: AssetImage('images/diamond.jpeg'),
+              ),
+              Text(
+                  'Innocent Mabuza',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 23.0,
                   color: Colors.white,
-                  child: Text('Container 1')
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              SizedBox(
-                height: 20.0,),
-              Container(
-                  height: 100.00,
-                  width: 100.00,
-                  color: Colors.green,
-                  child: Text('Container 2')
-              ),
-              Container(
-                  height: 100.00,
-                  width: 100.00,
-                  color: Colors.red,
-                  child: Text('Container 3')
-              ),
-              Container(
-                  height: 100.00,
-                  width: 100.00,
+              Text(
+                  'DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
                   color: Colors.white,
-                  child: Text('Container 4')
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.phone, color: Colors.teal,),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+27 78 235 457',
+                      style: TextStyle(
+                        fontFamily: 'SourceSanPro',
+                        fontSize: 18.1,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.email, color: Colors.teal,),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'innocent@mabuza.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSanPro',
+                        fontSize: 18.1,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ],
-          )
+          ),
         ),
       ),
     );
